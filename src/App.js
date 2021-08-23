50b064c56137e6ee1ab1a9d68a2529236db7214b
niyatip25 <niyatip25@gmail.com>
2021-08-23 17:24:40 +0530

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import * as React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Box from "@material-ui/core/Box";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Brightness3Icon from "@material-ui/icons/Brightness3";
+import Brightness7Icon from "@material-ui/icons/Brightness7";
+import Paper from "@material-ui/core/Paper";
+import { useState } from "react";
+import { ThemeProvider } from "@emotion/react";
+import { unstable_createMuiStrictModeTheme } from "@material-ui/core";
+import { createTheme } from "@material-ui/core/styles";
 
 function App() {
+  const [theme, setTheme] = useState(true);
+  const icon = !theme ? <Brightness7Icon /> : <Brightness3Icon />;
+  const light = {
+    palette: {
+      mode: "light",
+    },
+  };
+  const dark = {
+    palette: {
+      mode: "dark",
+    },
+  };
+
+  const appliedTheme = createTheme(theme ? light : dark);
+  // const appliedTheme = unstable_createMuiStrictModeTheme(theme ? palette.light : palette.dark);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={appliedTheme}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <IconButton
+              edge="end"
+              color="inherit"
+              aria-label="mode"
+              onClick={() => setTheme(!theme)}
+            >
+              {icon}
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Paper>
+          <div>
+            <Typography variant="h3">Hello!</Typography>
+            <IconButton
+              edge="end"
+              color="inherit"
+              aria-label="mode"
+              onClick={() => setTheme(!theme)}
+            >
+              {icon}
+            </IconButton>
+            <Typography>
+              Click on {!theme ? "Sun" : "Moon"} Icon to change to{" "}
+              {!theme ? "Light" : "Dark"} theme
+            </Typography>
+          </div>
+        </Paper>
+      </Box>
+    </ThemeProvider>
   );
 }
 
---
+++ src/dashbord.js
@@ -0,0 +1,161 @@
+import * as React from "react";
+import { useState } from "react";
+import { styled, useTheme } from "@material-ui/core/styles";
+import { createTheme } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import MuiDrawer from "@material-ui/core/Drawer";
+import MuiAppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import MapIcon from "@material-ui/icons/Map";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import SettingsIcon from "@material-ui/icons/Settings";
+import MailIcon from "@material-ui/icons/Mail";
+import Brightness3Icon from "@material-ui/icons/Brightness3";
+import Brightness7Icon from "@material-ui/icons/Brightness7";
+import { ThemeProvider } from "@emotion/react";
+import { Paper } from "@material-ui/core";
+
+const drawerWidth = 60;
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar)(({ theme }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  })
+}));
+
+const Drawer = styled(MuiDrawer)(() => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+}));
+
+export default function MiniDrawer() {
+  const [theme, setTheme] = useState(true);
+  const icon = !theme ? <Brightness7Icon /> : <Brightness3Icon />;
+  const light = {
+    palette: {
+      mode: "light",
+    },
+  };
+  const dark = {
+    palette: {
+      mode: "dark",
+    },
+  };
+
+  const appliedTheme = createTheme(theme ? light : dark);
+  return (
+    <ThemeProvider theme={appliedTheme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              sx={{
+                marginRight: "36px",
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            ></Typography>
+            <IconButton
+              edge="end"
+              color="inherit"
+              aria-label="mode"
+              onClick={() => setTheme(!theme)}
+            >
+              {icon}
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent">
+          <DrawerHeader></DrawerHeader>
+          <Divider />
+          <List>
+            <ListItem button>
+              <ListItemIcon sx={{minWidth:30}}>
+                <MapIcon />
+              </ListItemIcon>
+              <ListItemText />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon sx={{minWidth:30}}>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText />
+            </ListItem>
+          </List>
+          <Divider />
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: 3,
+            m: 3,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <DrawerHeader />
+          <Paper>
+            <Box component="form" noValidate sx={{ mt: 1, padding: 8 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="oktatenent"
+                label="Okta Tenent"
+                name="oktatenent"
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="apikey"
+                label="API Key"
+                id="apikey"
+              />
+              <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
+                Save
+              </Button>
+            </Box>
+          </Paper>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
